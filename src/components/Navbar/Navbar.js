5020c877015fe0af8ab88af73c20043310ffae75
Efren_Marin <efren45marin@gmail.com>
2023-01-05 05:22:35 -0800

    Initial build with a navbar and sub navbar.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar() {
+	return (
+    <>
+		<nav className='navbar'>
+			<ul>
+				<li id="name-icon">#EfrenMarin</li>
+				<li>
+					<a href='../docs/EfrenMarin Resume.pdf' target="_blank">RESUME</a>
+				</li>
+				<li>
+					<a href='https://github.com/efrenmarin45' target="_blank">GITHUB</a>
+				</li>
+				<li><a href="https://www.linkedin.com/in/efren-marin/" target="_blank">LINKEDIN</a></li>
+				<li><a href="https://efren45marin.medium.com" target="_blank">MEDIUM ARTICLES</a></li>
+				<li className="currentView">ABOUT ME</li>
+			</ul>
+		</nav>
+    <div className="subNav">
+      <p>Overview</p>
+    </div>
+    </>
+	);
+}
+
+export default Navbar;
